1d22c4c931fe4482a20c2c6896aa59b91f36f690
Holy Obina <obinakelle@gmail.com>
2025-05-15 20:18:04 +0800

    TEMP: Set DEBUG = True to see error

--- job/core/settings.py
+++ job/core/settings.py
@@ -5,7 +5,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = True
+
 ALLOWED_HOSTS = ['jobhive-56yk.onrender.com']
 
 
