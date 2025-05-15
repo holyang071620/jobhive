20f9bf4253c478e2f646fd348ecce5c277bcfe48
Holy Obina <obinakelle@gmail.com>
2025-05-15 19:23:24 +0800

    Restructure: move settings to core, fix manage.py, Procfile, and wsgi

--- job/accounts/apps.py
+++ job/accounts/apps.py
@@ -2,8 +2,9 @@ from django.apps import AppConfig
 
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'accounts'
+    name = 'job.accounts'
 
     def ready(self):
         # This ensures signals.py gets loaded when the app starts
-        import accounts.signals
+        import job.accounts.signals
+
